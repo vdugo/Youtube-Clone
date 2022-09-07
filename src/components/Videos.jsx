9b95dd846b9798d48b5a76651a045d8b8fa917df
Vincent <v3rdugo@yahoo.com>
2022-09-07 14:05:46 -0700

    add VideoCard and ChannelCard components

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const Videos = ({videos}) => {
-    console.log(videos)
-  return (
-    <div>Videos</div>
-  )
+import { Stack, Box } from '@mui/material'
+
+import { VideoCard, ChannelCard } from './'
+
+const Videos = ( {videos} ) => {
+    return (
+        <Stack 
+        direction="row" 
+        flexWrap="wrap"
+        justifyContent="start"
+        gap={2}
+        >
+            {videos.map((item, index) => (
+                <Box key={index}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
+    )
 }
 
 export default Videos
