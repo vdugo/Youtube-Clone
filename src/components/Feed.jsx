208e991eecc80965ac2cce44e388ae0c2a69afba
Vincent <v3rdugo@yahoo.com>
2022-09-07 13:19:19 -0700

    create Videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from './Sidebar'
 
 const Feed = () => {
   return (
@@ -10,6 +10,11 @@ const Feed = () => {
         <Sidebar />
         <Typography className='copyright' variant='body2' sx={ {mt: 1.5, color: '#fff'} }>
           Copyright 2022
+        </Typography>
+      </Box>
+      <Box p={2} sx={ {overflowY: 'auto', height: '90vh', flex: 2} }>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={ {color: 'white'} }>
+          New <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
       </Box>
     </Stack>
