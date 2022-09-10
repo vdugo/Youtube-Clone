da11a35a42b8e3a7a31c9de51775ff1cd630ea4c
Vincent <v3rdugo@yahoo.com>
2022-09-10 16:40:19 -0700

    card thumbnails rendering properly

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const VideoCard = () => {
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
+
+const VideoCard = ({ video: { id: {videoId}, snippet } }) => {
+  console.log(videoId, snippet)
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia 
+        image={snippet?.thumbnails?.high?.url}
+        alt={snippet?.title}
+        sx={{ width: 358, height: 180}}
+        >
+
+        </CardMedia>
+      </Link>
+    </Card>
   )
 }
 
